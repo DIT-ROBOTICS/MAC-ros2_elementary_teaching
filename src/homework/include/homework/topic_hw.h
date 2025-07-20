c9fe7d011a04d57a45aa21d9494379a2b429cdb5
Ramie119 <ramie0119@gapp.nthu.edu.tw>
2025-07-21 01:01:22 +0800

    commit hw1

--- src/homework/include/homework/topic_hw.h
+++ src/homework/include/homework/topic_hw.h
@@ -1,31 +1,32 @@
 #pragma once
-// TODO: include message type for the topic
 
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <string>
 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int64.hpp"
+
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
-class pub : public rclcpp::Node{
-
-  public:
+class pub : public rclcpp::Node {
+public:
     pub();
 
-  private:
+private:
     void timer_callback();
-
-    // TODO: create a publisher and timer for the topic
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr publisher_;
+    int64_t count_ = 0;
 };
 
-class sub : public rclcpp::Node{
-
-  public:
+class sub : public rclcpp::Node {
+public:
     sub();
 
-  private:
+private:
     void topic_callback(const std_msgs::msg::Int64 & msg);
-    // TODO: create a subscriber for the topic
+    rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr subscription_;
 };
--- src/homework/src/subscription.cpp
+++ src/homework/src/subscription.cpp
@@ -1,19 +1,17 @@
 #include "homework/topic_hw.h"
 
-sub::sub() : Node("TODO: node_name"){  
-
-     // TODO: setup a subscriber and timer for the subscriber
+sub::sub() : Node("Subscriber") {
+    subscription_ = this->create_subscription<std_msgs::msg::Int64>(
+        "/topic", 10, std::bind(&sub::topic_callback, this, _1));
 }
 
-void sub::topic_callback(const your_msgs::msg::xxx & msg){
-
-   // TODO: set up the callback function for the subscriber
+void sub::topic_callback(const std_msgs::msg::Int64 & msg) {
+    RCLCPP_INFO(this->get_logger(), "Received: '%ld'", msg.data);
 }
 
-int main(int argc, char * argv[]){
-  
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<sub>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<sub>());
+    rclcpp::shutdown();
+    return 0;
 }
