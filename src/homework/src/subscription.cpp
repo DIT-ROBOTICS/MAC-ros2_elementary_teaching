7be70146882793b79d530dff2e68d168c91258c0
ling-stu <royling@gapp.nthu.edu.tw>
2025-07-22 01:07:08 +0800

    TEST:commit_test2

--- src/homework/src/subscription.cpp
+++ src/homework/src/subscription.cpp
@@ -1,13 +1,22 @@
 #include "homework/topic_hw.h"
 
-sub::sub() : Node("TODO: node_name"){  
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+sub::sub() : Node("subscriber_node") {  
 
      // TODO: setup a subscriber and timer for the subscriber
+    subscriber_ = this->create_subscription<std_msgs::msg::Int64>(
+        "topic_name", 10, std::bind(&sub::topic_callback, this, std::placeholders::_1));
+    // The subscriber will call topic_callback whenever a new message is received
+    RCLCPP_INFO(this->get_logger(), "Subscriber node initialized and waiting for messages.");
 }
 
 void sub::topic_callback(const your_msgs::msg::xxx & msg){
 
    // TODO: set up the callback function for the subscriber
+    RCLCPP_INFO(this->get_logger(), "Received message: '%ld'", msg.data);
+
 }
 
 int main(int argc, char * argv[]){
